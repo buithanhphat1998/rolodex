2acbc7ab754376f58b7023bf986a98854b790c7e
hoanghagiabao <hoanghagiabao@gmail.com>
2025-09-24 00:23:28 -0700

    Update contact.py
    comment for contact

--- contact.py
+++ contact.py
@@ -5,14 +5,26 @@ class Contact:
     address: str
     city: str
     zip: str
-
+    # Constructor
     def __init__(self,fn,ln,ph,addr,city,zip):
+        """
+        INPUT:
+            fn   (str): first name
+            ln   (str): last name
+            ph   (str): phone number
+            addr (str): street address
+            city (str): city
+            zip  (str): zip code
+        OUTPUT:
+            Stores all provided values.
+        """
         self.f_name = fn
         self.l_name = ln
         self.phone = ph
         self.address = addr
         self.city = city
         self.zip = zip
+    # Less-than operator for sorting
     def __lt__(self, other):
         if(self.l_name == other.l_name):
             if(self.f_name < other.f_name):
@@ -21,14 +33,16 @@ class Contact:
                 return False
         else:
             return True if self.l_name < other.l_name else False
+    # String representation for display
     def __str__(self):
         info = f"""{self.l_name}
-{self.f_name}
-{self.phone}
-{self.address}
-{self.city}
-{self.zip}
-"""
+                   {self.f_name}
+                   {self.phone}
+                   {self.address}
+                   {self.city}
+                   {self.zip}
+                             """
         return info
+    # Official representation for saving
     def __repr__(self):
         return f'{self.f_name},{self.l_name},{self.phone},{self.address},{self.city},{self.zip}\n'
